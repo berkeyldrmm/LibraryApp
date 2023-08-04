8656e984218e3c9a91ac31fbfb9677b9156422d1
berke <berke.yildirimm44@outlook.com>
2023-08-04 10:58:49 +0300

    Add project files.

---
+++ EntityLayer/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string BookAuthor { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsInLibrary { get; set; }
+    }
+}
